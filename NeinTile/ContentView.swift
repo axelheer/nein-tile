5bdee8d306b86030bb4d8bec4043631e750643bd
Axel Heer <axel@heer.io>
2020-05-17 13:55:21 +0200

    Initial Commit

---
+++ NeinTile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeinTile
+//
+//  Created by Axel on 17.05.20.
+//  Copyright © 2020 Axel Heer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
